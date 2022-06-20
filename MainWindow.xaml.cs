bfc85a7618832b4d65a71a62aeff93567c64cfd0
MarekWilk <maro_will@wp.pl>
2022-06-20 23:23:07 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace projekt_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Logowanie(object sender, RoutedEventArgs e)
+        {
+            Logowanie logowanie = new Logowanie();
+            logowanie.Show();
+        }
+        private void Button_Click_Rejestracja(object sender, RoutedEventArgs e)
+        {
+            Rejestracja rejestracja = new Rejestracja();
+            rejestracja.Show();
+        }
+    }
+}
